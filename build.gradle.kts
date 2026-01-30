867cbe4251fbcbf889523755bafb3279e2db79bd
Mr. Yusuph Moris💼📚 <yusuphmoris03@gmail.com>
2026-01-30 09:48:55 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0") // Example AGP Versio
+        classpath("com.android.tools.build:gradle:8.1.0") // Example AGP Version
         // Add other classpaths if needed
     }
 }
