b21ee3f654c57c48e8d63034ac97ae6ec2e6a04b
Mr. Yusuph Moris💼📚 <yusuphmoris03@gmail.com>
2026-02-06 16:03:50 +0300

    Update AppDatabase.kt

--- app/src/main/java/com/example/crudapplication/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/crudapplication/data/database/AppDatabase.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.studentcrudapp.data.dao.Stu
+import com.example.studentcrudapp.data.dao.Stud
 import com.example.studentcrudapp.data.entity.Student
 
 @Database(entities = [Student::class], version = 1)
