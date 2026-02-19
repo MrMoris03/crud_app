a4fafa8f300ab7561bfa4064c998491c052204ae
Mr. Yusuph Moris💼📚 <yusuphmoris03@gmail.com>
2026-02-19 23:28:12 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modul
+// Top-level build file where you can add configuration options common to all sub-projects/module
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
